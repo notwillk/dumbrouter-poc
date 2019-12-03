71b1245b7ab5be55fafeeb40ce35080bd8c0723f
William Klancnik <william@klancnik.com>
2019-12-03 05:31:38 -0800

    Move named paths to standalone file

--- src/AppProviders.js
+++ src/AppProviders.js
@@ -2,17 +2,7 @@ import React, { Suspense } from 'react';
 import WindowLocation from './WindowLocation';
 import DumbRouter from './DumbRouter';
 import App from './App';
-
-const namedPaths = {
-  page1: '/page1',
-  page2: {
-    path: '/page2',
-    sensitive: true
-  },
-  page3: {
-    path: '/page3/:id'
-  },
-};
+import * as namedPaths from './routes';
 
 const routes = [
   ['page1', { abc:123 }],
@@ -21,7 +11,7 @@ const routes = [
   ['/page4', { abc:0 }]
 ];
 
-const notFound = {};
+const notFound = { nothing: true };
 
 const AppProviders = () => (
   <Suspense fallback={<div>Loading...</div>}>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export const page1 = '/page1';
+
+export const page2 = {
+  path: '/page2',
+  sensitive: true
+};
+export const page3 = {
+  path: '/page3/:id'
+};
