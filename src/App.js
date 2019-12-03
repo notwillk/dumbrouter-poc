c47bae77af3aa56de9678aaac983430ed45be41b
William Klancnik <william@klancnik.com>
2019-12-03 06:07:37 -0800

    Move navigation to its own component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { useWindowLocationPathname, usePushPath } from './WindowLocation';
-import { useOutboundRoute, useFirstMatch } from './DumbRouter';
+import { useWindowLocationPathname } from './WindowLocation';
+import { useFirstMatch } from './DumbRouter';
+import Navigation from './Navigation';
 import FallbackComponent from './FallbackComponent';
 
 const App = () => {
-  const pushPath = usePushPath();
-
-  const link1 = useOutboundRoute('page1');
-  const link2 = useOutboundRoute('page2');
-  const link3 = useOutboundRoute('page3');
-  const link4 = useOutboundRoute('/page4');
-
   const pathname = useWindowLocationPathname();
-
   const { Component } = useFirstMatch(pathname);
 
   return (
     <div>
-      <ul>
-        <li><button onClick={() => pushPath(link1())}>page 1</button></li>
-        <li><button onClick={() => pushPath(link2())}>page 2</button></li>
-        <li><button onClick={() => pushPath(link3({ id: 12345 }))}>page 3</button></li>
-        <li><button onClick={() => pushPath(link4())}>page 4</button></li>
-        <li><button onClick={() => pushPath("/gibberish")}>unknown</button></li>
-      </ul>
+      <Navigation />
       { Component ? <Component /> : <FallbackComponent /> }
     </div>
   );
---
+++ src/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { usePushPath } from './WindowLocation';
+import { useOutboundRoute } from './DumbRouter';
+
+const Navigation = () => {
+  const pushPath = usePushPath();
+
+  const link1 = useOutboundRoute('page1');
+  const link2 = useOutboundRoute('page2');
+  const link3 = useOutboundRoute('page3');
+  const link4 = useOutboundRoute('/page4');
+
+  return (
+    <ul>
+      <li><button onClick={() => pushPath(link1())}>page 1</button></li>
+      <li><button onClick={() => pushPath(link2())}>page 2</button></li>
+      <li><button onClick={() => pushPath(link3({ id: 12345 }))}>page 3</button></li>
+      <li><button onClick={() => pushPath(link4())}>page 4</button></li>
+      <li><button onClick={() => pushPath("/gibberish")}>unknown</button></li>
+    </ul>
+  );
+}
+
+export default Navigation;
